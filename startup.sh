26f829da355c381cb28787d3e76557f021f8d02f
vksinwar <vksinwar0001@gmail.com>
2024-09-25 02:05:55 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gunicorn --bind=0.0.0.0 --timeout 600 --workers 4 --worker-class uvicorn.workers.UvicornWorker app:app
+#gunicorn --bind=0.0.0.0 --timeout 600 --workers 4 --worker-class uvicorn.workers.UvicornWorker app:app
+gunicorn --bind=0.0.0.0 --timeout 600 app:app -k uvicorn.workers.UvicornWorker
